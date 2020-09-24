26f141f67c284b42d0dc0f3d9f71972a50ac5f1b
rohit-gupta-od <rohit.gupta1@officedepot.com>
2020-09-24 16:23:49 +0530

    adding info about mutable/immutable datataype and minor restructuring

--- notes/javascript/hoisting.js
+++ notes/javascript/hoisting.js
@@ -1,4 +1,7 @@
-// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution. Inevitably, this // means that no matter where functions and variables are declared, they are moved to the top of their scope regardless of whether their scope is global or // local
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of 
+// their scope before code execution. Inevitably, this 
+// means that no matter where functions and variables are declared, they are moved to the top of their
+// scope regardless of whether their scope is global or local
 
 // function hoisting1() {
 //     console.log('.....', a1);  // throws error a1 not defined
